24ab0c22c1873828bea618d4805ff2fe7138a949
Robera Tesfaye <roberatesfaye843@gmail.com>
2025-02-11 20:31:40 +0300

    login and registration

--- back-end/repository_system/users/urls.py
+++ back-end/repository_system/users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import GuestRegistrationView, VerifyGuestView
+from .views import GuestRegistrationView, VerifyGuestView, StudentRegistrationView, EmployeeRegistrationView,LoginView
+
 
 urlpatterns = [
     path('register-guest/', GuestRegistrationView.as_view(), name='register-guest'),
     path('verify-guest/<str:token>/', VerifyGuestView.as_view(), name='verify-guest'),
+    path('register-student/', StudentRegistrationView.as_view(), name='register-student'),
+    path('register-employee/', EmployeeRegistrationView.as_view(), name='register-employee'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
