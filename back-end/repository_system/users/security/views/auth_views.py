d1bcd51a78a5ee765e229916ce4bd3e65747c62c
= <=>
2025-02-13 07:16:11 +0300

    fix the jwt

--- back-end/repository_system/users/security/views/auth_views.py
+++ back-end/repository_system/users/security/views/auth_views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from ..serializers.auth_serializers import LoginSerializer, TokenRefreshSerializer
+from ..serializers.auth_serializers import LoginSerializer # TokenRefreshSerializer
 
 class LoginView(APIView):
     permission_classes = [AllowAny]
