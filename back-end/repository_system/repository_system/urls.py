f37e63e29d226a4fcbf16d245d00eebef4d8fe57
= <=>
2025-03-18 14:09:17 +0300

    linked the admin dashbord to the project and works as well test it as well and make

--- back-end/repository_system/repository_system/urls.py
+++ back-end/repository_system/repository_system/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from .wellcomPage import admin_welcome
+from .staticImort.wellcomPage import admin_welcome
+from .staticImort.dashbord import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('Authentication.urls')),
     path('api/files/', include('files.urls')),
     path('', admin_welcome, name='admin_welcome'),
+    # path('dashbord/', dashboard , name='dashbord'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
